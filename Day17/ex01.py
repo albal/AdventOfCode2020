0a9ba4bd7d9ac1b345e322b7493ba57d49b775f4
Al West <al@tsew.net>
2020-12-18 13:50:51 +0000

    Day 17 (0) and 18

---
+++ Day17/ex01.py
@@ -0,0 +1,5 @@
+with open('input17.txt', 'r') as file:
+    data = [list(line.strip()) for line in file]
+
+for line in data:
+    print(line)
---
+++ Day18/ex02.py
@@ -0,0 +1,22 @@
+# https://www.programiz.com/python-programming/precedence-associativity
+import re
+
+with open('input18.txt', 'r') as file:
+    data = file.read().split('\n')
+
+
+class mySwap(int):
+    def __add__(self, other):
+        return mySwap(int(self) + other)
+
+    def __or__(self, other):
+        return mySwap(int(self) * other)
+
+
+def parse(calculation):
+    calculation = re.sub(r"(\d+)", r"mySwap(\1)", calculation)
+    calculation = calculation.replace("*", "|")
+    return eval(calculation)
+
+
+print("Sun:", sum(map(parse, data)))
